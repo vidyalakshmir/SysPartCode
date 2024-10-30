9cc0962538c7a5357470b7ddcb7b0305fb5e3226
Fatih Durmaz <mail.fatih.durmaz@gmail.com>
2024-10-30 19:18:00 +0000

    adjusted tests
    Signed-off-by: Fatih Durmaz <mail.fatih.durmaz@gmail.com>

---
+++ test/Readme.md
@@ -0,0 +1,13 @@
+### Build test binaries
+
+```
+make
+```
+
+### Run Tests
+
+```
+# chmod +x hello*/test.sh
+# chmod +x run_all_tests.sh
+./run_all_tests.sh
+```
--- test/helloDynamic/test.sh
+++ test/helloDynamic/test.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 echo "Running dynamic analysis on helloDynamic/main "
 
-# Define paths
 MAIN_EXECUTABLE="$PWD/helloDynamic/main"
 OUTPUT_DIR="$PWD/helloDynamic"
 ANALYSIS_DIR="../analysis/app/src/dlanalysis/static"
 
-# Change to the analysis directory and run the analysis script
-(cd $ANALYSIS_DIR && ./run_dlanalysis.sh $MAIN_EXECUTABLE $OUTPUT_DIR)
+if [ ! -f helloDynamic/dlsym_static.txt ]; then
+    (cd $ANALYSIS_DIR && ./run_dlanalysis.sh $MAIN_EXECUTABLE $OUTPUT_DIR)
+fi
 
-# Check for printHello in the output
-grep -q 'SYM printHello' helloDynamic/dlsym_static.txt
+grep -q 'printHello' helloDynamic/dlsym_static.txt
 printHello_check=$?
 
-# Check for libhello.so in the output
-grep -q 'SYM hello/libhello.so' helloDynamic/dlopen_static.txt
+grep -q 'libhello.so' helloDynamic/dlopen_static.txt
 libhello_check=$?
 
-# Evaluate the results
 if [ $printHello_check -eq 0 ] && [ $libhello_check -eq 0 ]; then
     echo "Test passed: Successfully found printHello in dlsym_static.txt and libhello.so in dlopen_static.txt."
     exit 0
